b1e2f415cdbd505b9c5b07b5bb44a08fefbc2e52
Daniil2K6 <FoxmanOriginal@gmail.com>
2025-06-13 19:38:46 +0300

    fix: добавлены новые рассы и отредактированы некоторые ранее существующие, также с классами

--- src/constants/races.ts
+++ src/constants/races.ts
@@ -19,34 +19,34 @@ export const RACES: Record<string, Race> = {
       strength: 0.2, // очень слабые физически
       dexterity: 1.3,
       constitution: 0.4, // очень хрупкие
-      intelligence: 1.7,
+      intelligence: 1.4,
       wisdom: 1.4,
       charisma: 1.5,
-      magic: 2.0 // сильная магия
+      magic: 2.1 // сильная магия
     }
   },
   dwarf: {
     name: 'Дварф',
     multipliers: {
-      strength: 1.8,
-      dexterity: 0.7,
-      constitution: 2.0, // очень живучие
-      intelligence: 0.9,
+      strength: 1.5,
+      dexterity: 0.8,
+      constitution: 1.4, // очень живучие
+      intelligence: 1.1,
       wisdom: 1.3,
-      charisma: 0.5,
-      magic: 0.8
+      charisma: 0.7,
+      magic: 1.3
     }
   },
   gnome: {
     name: 'Гном',
     multipliers: {
       strength: 1.4,
-      dexterity: 1.1,
-      constitution: 1.5,
+      dexterity: 1.3,
+      constitution: 1.6,
       intelligence: 1.0,
       wisdom: 1.2,
-      charisma: 0.7,
-      magic: 1.0
+      charisma: 0.5,
+      magic: 1.1
     }
   },
   orc: {
@@ -76,49 +76,49 @@ export const RACES: Record<string, Race> = {
   goblin: {
     name: 'Гоблин',
     multipliers: {
-      strength: 0.3, // мем-дебаф
-      dexterity: 1.7, // ловкие
-      constitution: 0.3, // мем-дебаф
-      intelligence: 0.5, // мем-дебаф
-      wisdom: 5.0, // мем-бонус
-      charisma: 0.2, // мем-дебаф
-      magic: 0.2 // мем-дебаф
+      strength: 0.4,
+      dexterity: 1.0, 
+      constitution: 0.5, 
+      intelligence: 0.4,
+      wisdom: 5.0,
+      charisma: 0.1,
+      magic: 0.9
     }
   },
   demon_power: {
     name: 'Демон Силы',
     multipliers: {
-      strength: 3.0,
-      dexterity: 1.0,
-      constitution: 2.0,
-      intelligence: 0.8,
-      wisdom: 0.7,
-      charisma: 1.0,
+      strength: 2.2,
+      dexterity: 0.7,
+      constitution: 1.8,
+      intelligence: 0.7,
+      wisdom: 0.5,
+      charisma: 0.7,
       magic: 1.2
     }
   },
   demon_lust: {
     name: 'Демон Похоти',
     multipliers: {
-      strength: 0.8,
-      dexterity: 1.2,
-      constitution: 0.9,
-      intelligence: 1.3,
-      wisdom: 0.8,
+      strength: 0.4,
+      dexterity: 1.3,
+      constitution: 0.3,
+      intelligence: 1.0,
+      wisdom: 0.7,
       charisma: 3.0, // супер-обаяние
-      magic: 1.5
+      magic: 2.0
     }
   },
   demon_fire: {
     name: 'Демон Огня',
     multipliers: {
-      strength: 1.3,
+      strength: 0.8,
       dexterity: 1.1,
-      constitution: 1.1,
+      constitution: 1.0,
       intelligence: 1.0,
-      wisdom: 0.9,
+      wisdom: 0.8,
       charisma: 1.0,
-      magic: 2.5 // сильная магия
+      magic: 2.0 // сильная магия
     }
   },
   angel: {
@@ -130,7 +130,67 @@ export const RACES: Record<string, Race> = {
       intelligence: 1.5,
       wisdom: 2.0,
       charisma: 2.0,
-      magic: 2.0
+      magic: 2.0  
+    }
+  },
+  vampire: {
+    name: 'Вампир',
+    multipliers: {
+      strength: 0.6,
+      dexterity: 1.5,
+      constitution: 0.7,
+      intelligence: 1.4,
+      wisdom: 1.1,
+      charisma: 2.0,
+      magic: 1.7
+    }
+  },
+  dark_elf: {
+    name: 'Тёмный эльф',
+    multipliers: {
+      strength: 0.4,
+      dexterity: 1.5,
+      constitution: 0.3,
+      intelligence: 1.6,
+      wisdom: 1.2,
+      charisma: 1.3,
+      magic: 2.3
+    }
+  },
+  gnoll: {
+    name: 'Гнолл',
+    multipliers: {
+      strength: 1.7,
+      dexterity: 1.1,
+      constitution: 1.5,
+      intelligence: 0.5,
+      wisdom: 0.7,
+      charisma: 0.6,
+      magic: 0.4
+    }
+  },
+  beastfolk: {
+    name: 'Зверолид',
+    multipliers: {
+      strength: 1.3,
+      dexterity: 1.4,
+      constitution: 1.2,
+      intelligence: 0.8,
+      wisdom: 1.0,
+      charisma: 0.9,
+      magic: 0.7
+    }
+  },
+  arachnid: {
+    name: 'Арахнид',
+    multipliers: {
+      strength: 0.8,
+      dexterity: 1.7,
+      constitution: 1.0,
+      intelligence: 1.3,
+      wisdom: 1.0,
+      charisma: 0.5,
+      magic: 1.5
     }
   }
 };
